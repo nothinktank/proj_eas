e7aaf0015450311d739ce3d81f953a1724d84a10
shangwang <wangshangming10@gmail.com>
2024-05-03 11:59:22 -0400

    reference container div

---
+++ proj_eas.js
@@ -0,0 +1,17 @@
+//create reference to container div
+let containerDiv = document.querySelector("#container")
+
+let testDiv = document.createElement("div");
+
+let gridSize = 16; //16 x 16 grid
+
+//create 16 divs and append them horizontally 
+
+//create 16 horizontal divs and append them vertically
+
+
+
+
+for (n = 1; n < 16 ; n++){
+
+}
