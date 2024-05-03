e98f2d155a81bdb09f87f21fbc186ef80cfb9f17
shangwang <wangshangming10@gmail.com>
2024-05-03 14:01:12 -0400

    create 1 horizontal row of 16 divs in the container div

--- proj_eas.js
+++ proj_eas.js
@@ -1,11 +1,20 @@
 //create reference to container div
 let containerDiv = document.querySelector("#container")
 
+/*
 let testDiv = document.createElement("div");
+testDiv.id = "row";
+containerDiv.appendChild(testDiv);
+*/
 
 let gridSize = 16; //16 x 16 grid
 
 //create 16 divs and append them horizontally 
+for (n = 0; n < 16 ; n++){
+ let rowDiv = document.createElement(`div`);
+ rowDiv.id = "row";
+ containerDiv.appendChild(rowDiv);
+}
 
 //create 16 horizontal divs and append them vertically
 
