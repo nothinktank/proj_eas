13ae7e1e8f4adea57c5b383a1dc0ecb680f785b5
shangwang <wangshangming10@gmail.com>
2024-05-03 15:48:18 -0400

    created 16*16 grid

--- proj_eas.js
+++ proj_eas.js
@@ -10,11 +10,23 @@ containerDiv.appendChild(testDiv);
 let gridSize = 16; //16 x 16 grid
 
 //create 16 divs and append them horizontally 
-for (n = 0; n < 16 ; n++){
- let rowDiv = document.createElement(`div`);
- rowDiv.id = "row";
- containerDiv.appendChild(rowDiv);
+
+//create a div to include the entire row of 16 horizontal divs
+
+for (i = 0; i < 16; i++){
+  let columnDiv = document.createElement(`div`);
+  columnDiv.id = `column`
+
+  for (n = 0; n < 16 ; n++){
+    let rowDiv = document.createElement(`div`);
+    rowDiv.id = `row`;
+    columnDiv.appendChild(rowDiv);
+  }
+  containerDiv.appendChild(columnDiv);
+
 }
+
+
 
 //create 16 horizontal divs and append them vertically
 
